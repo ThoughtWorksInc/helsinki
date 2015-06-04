a9f2d5154a350cb06de78a7fdf3987b3b48d0dfb
Chris Cheshire <chris.cheshire@gmail.com>
2015-06-04 16:09:19 +0100

    Apply some basic jade and css

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import argparse
 from flask import Flask, render_template, request, jsonify
+from pprint import pprint
 
 from data.decisions import import_decision_data, agenda_item_to_municipal_action
 from data.es import index_decision, find_decisions, configure
-
 
 app = Flask(__name__)
 app.jinja_env.add_extension('pyjade.ext.jinja.PyJadeExtension')
@@ -11,31 +11,40 @@ app.jinja_env.add_extension('pyjade.ext.jinja.PyJadeExtension')
 
 @app.route("/")
 def home():
-  return render_template('main.jade')
+    return render_template('index.jade')
 
 
 @app.route("/search", methods=["GET"])
 def search_decisions():
-  criteria = request.args.get("q")
-  if criteria:
-    return jsonify(results=find_decisions(criteria.strip()))
-  return "?q=SEARCH_TERM"
+    criteria = request.args.get("q")
+    if criteria:
+        criteria_stripped = criteria.strip()
+        results = find_decisions(criteria_stripped)
+        pprint(results)
+        return render_template('results.jade',
+                                results=results,
+                                searchTerm=criteria_stripped)
+    return ""
+
+
+@app.route("/decision")
+def decision():
+    return render_template('decision.jade')
 
 
 if __name__ == "__main__":
-  parser = argparse.ArgumentParser()
-  parser.add_argument("--reindex", action="store_true")
-  parser.add_argument("--debug", action="store_true")
-  args = parser.parse_args()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--reindex", action="store_true")
+    parser.add_argument("--debug", action="store_true")
+    args = parser.parse_args()
 
-  if args.reindex:
-    print "Indexing API data..."
-    configure()
-    decisions = import_decision_data()
-  
-    for d in decisions.get("objects"):
-      index_decision(agenda_item_to_municipal_action(d))
+    if args.reindex:
+        print "Indexing API data..."
+        configure()
+        decisions = import_decision_data()
 
-  app.debug = bool(args.debug)
-  app.run()
+        for d in decisions.get("objects"):
+            index_decision(agenda_item_to_municipal_action(d))
 
+    app.debug = bool(args.debug)
+    app.run()
