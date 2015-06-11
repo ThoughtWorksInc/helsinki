ef5350c50fefe3b08f08e4cd33d0a2e0d74bfb7d
John Cowie <john.a.cowie@gmail.com>
2015-06-11 10:19:20 +0100

    John - added a basic setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup (
+	name='helsinki',
+	version='0.0.1',
+	description='Notifications for helsinki citizens',
+	license='MIT',
+	classifiers=[
+		'Programming Language :: Python :: 2.7'	
+	],
+	packages=find_packages()
+
+)
