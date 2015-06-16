40b8a2cdc6b8fb6c34200b79d7a8ce75ab98a0cf
Rob Speller <rspeller@thoughtworks.com>
2015-06-16 13:30:37 +0100

    RS & GR | Use rsync in deploy.sh to create target directory if it doesn't exist

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 tar -cvzf $TAR $DIR
-scp dist/* $REMOTE_USER@$SERVER_IP:/var/helsinki/dist
+rsync -r dist/* $REMOTE_USER@$SERVER_IP:/var/helsinki/dist
 ssh $REMOTE_USER@$SERVER_IP "sudo docker stop helsinki && sudo docker rm helsinki && sudo docker run -d -v /var/helsinki/dist:/var/helsinki/dist -p 5000:5000 --name helsinki --link mongo:mongo --link elasticsearch:elasticsearch  python:2.7 bash -c 'pip install /var/helsinki/dist/helsinki-0.0.1.tar.gz && helsinki --reindex'
