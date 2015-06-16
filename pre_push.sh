0908c013fb11fe920b616d6978877ca95ee67310
Rob Speller <rspeller@thoughtworks.com>
2015-06-16 11:15:04 +0100

    RS + GR - Add requirements to sdist

--- pre_push.sh
+++ pre_push.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 pip install -r requirements.txt
+pip install -r requirements_for_tests.txt
 nosetests
 pep8 .
--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 import os
 from setuptools import setup, find_packages
 
+requirements = [line.rstrip('\n') for line in open('requirements.txt')]
+
 setup(
     name='helsinki',
     version='0.0.1',
@@ -11,6 +13,7 @@ setup(
     ],
     packages=find_packages(),
     include_package_data=True,
+    install_requires=requirements,
     entry_points={
         'console_scripts': ['helsinki=helsinki.main:run_app']
     }
