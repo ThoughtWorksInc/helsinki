02f841d36630901a0487e850d647f97a71f88139
Rob Speller <rspeller@thoughtworks.com>
2015-06-16 13:54:54 +0100

    RS & GR | Edit deploy.sh to not fail when helsinki container doesn't exist

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,8 @@
 tar -cvzf $TAR $DIR
 ssh $REMOTE_USER@$SERVER_IP "mkdir -p /var/helsinki/dist"
 scp dist/* $REMOTE_USER@$SERVER_IP:/var/helsinki/dist
-ssh $REMOTE_USER@$SERVER_IP "sudo docker run --rm=true -d -v /var/helsinki/dist:/var/helsinki/dist -p 5000:5000 --name helsinki --link mongo:mongo --link elasticsearch:elasticsearch  python:2.7 bash -c 'pip install /var/helsinki/dist/helsinki-0.0.1.tar.gz && helsinki --reindex'"
+ssh $REMOTE_USER@$SERVER_IP << EOF
+  sudo docker stop helsinki || echo "Failed to stop helsinki"
+  sudo docker rm helsinki || echo "Failed to remove helsinki"
+  sudo docker run -d -v /var/helsinki/dist:/var/helsinki/dist -p 5000:5000 --name helsinki --link mongo:mongo --link elasticsearch:elasticsearch  python:2.7 bash -c 'pip install /var/helsinki/dist/helsinki-0.0.1.tar.gz && helsinki --reindex'"
+EOF
