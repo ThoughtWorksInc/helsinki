e6233355e31508fee91b6e16511145f4b66ca6e4
Rob Speller <rspeller@thoughtworks.com>
2015-06-16 15:11:23 +0100

    RS & GR | Try changing port when starting python docker container

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,5 @@ scp dist/* $REMOTE_USER@$SERVER_IP:/var/helsinki/dist
 ssh $REMOTE_USER@$SERVER_IP << EOF
   sudo docker stop helsinki || echo "Failed to stop helsinki"
   sudo docker rm helsinki || echo "Failed to remove helsinki"
-  sudo docker run -d -v /var/helsinki/dist:/var/helsinki/dist -p 5000:5000 --name helsinki --link mongo:mongo --link elasticsearch:elasticsearch  python:2.7 bash -c 'pip install /var/helsinki/dist/helsinki-0.0.1.tar.gz && helsinki --reindex'
+  sudo docker run -d -v /var/helsinki/dist:/var/helsinki/dist -p 127.0.0.1/:5000:5000 --name helsinki --link mongo:mongo --link elasticsearch:elasticsearch  python:2.7 bash -c 'pip install /var/helsinki/dist/helsinki-0.0.1.tar.gz && helsinki --reindex'
 EOF
