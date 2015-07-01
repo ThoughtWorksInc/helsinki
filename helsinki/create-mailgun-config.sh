0d21c2c1bdfb6ee1371d182831e0d38b082556f6
John Cowie <jcowie@thoughtworks.com>
2015-07-01 17:21:53 +0100

    CF + JC | Created script to generate mailgun.json config

---
+++ helsinki/create-mailgun-config.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [ -z "$MAILGUN_API_KEY" ]; then
+  echo "MAILGUN_API_KEY is not set"
+  exit 1;
+fi
+
+if [ -z "$MAILGUN_SANDBOX_ID" ]; then
+  echo "MAILGUN_SANDBOX_ID is not set"
+  exit 1;
+fi
+
+echo "{
+  \"key\": \"$MAILGUN_API_KEY\",
+  \"post_url\": \"https://api.mailgun.net/v3/$MAILGUN_SANDBOX_ID\",
+  \"sandbox\": \"$MAILGUN_SANDBOX_ID\"
+}" > mailgun.json
