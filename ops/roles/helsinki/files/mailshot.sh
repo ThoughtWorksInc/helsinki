d48315529a31ac6063087d245a74c05052b81ea4
John Cowie <jcowie@thoughtworks.com>
2015-07-01 16:34:01 +0100

    CF + JC | Added mongo links to mailshot and reindex scripts

--- ops/roles/helsinki/files/mailshot.sh
+++ ops/roles/helsinki/files/mailshot.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sudo docker run -v /var/helsinki/dist:/var/helsinki/dist --name helsinki_mailshot --link elasticsearch:elasticsearch python:2.7 bash -c 'pip install /var/helsinki/dist/helsinki-0.0.1.tar.gz && helsinki --mailshot'
+sudo docker run -v /var/helsinki/dist:/var/helsinki/dist --name helsinki_mailshot --link elasticsearch:elasticsearch --link mongo:mongo python:2.7 bash -c 'pip install /var/helsinki/dist/helsinki-0.0.1.tar.gz && helsinki --mailshot'
 sudo docker rm helsinki_mailshot
