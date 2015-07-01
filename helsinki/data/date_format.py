983dc2dbab8e2d6f1d20ac47f16960fedc435196
Chris Cheshire <chris.cheshire@gmail.com>
2015-07-01 15:00:23 +0100

    Fix date diff
    Today and Tomorrow didn't work as the diff wasn't an integer

--- helsinki/data/date_format.py
+++ helsinki/data/date_format.py
@@ -2,13 +2,12 @@ from datetime import datetime
 
 
 def friendly_day(ugly_date):
-    print ugly_date
     date = _parse_date(ugly_date)
     diff = datetime.now().date() - date
 
-    if diff == 0:
+    if diff.days == 0:
         return "Today"
-    elif diff == -1:
+    elif diff.days == -1:
         return "Yesterday"
     else:
         return date.strftime("%A")
