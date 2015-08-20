0de63fb6befb1ce7d6d57a982831b85111ce2c0b
John Cowie <jcowie@thoughtworks.com>
2015-08-20 15:53:53 +0100

    JC | updated deployment script to use env file

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 ssh $REMOTE_USER@$SERVER_IP "mkdir -p /var/helsinki/dist"
+ssh $REMOTE_USER@$SERVER_IP "mkdir -p /var/helsinki/config"
 scp dist/* $REMOTE_USER@$SERVER_IP:/var/helsinki/dist
+scp config.env $REMOTE_USER@$SERVER_IP:/var/helsinki/config
 ssh $REMOTE_USER@$SERVER_IP << EOF
   sudo docker stop helsinki || echo "Failed to stop helsinki container"
   sudo docker rm helsinki || echo "Failed to remove helsinki"
-  sudo docker run -d -v /var/helsinki/dist:/var/helsinki/dist -p 127.0.0.1:5000:5000 --name helsinki --link mongo:mongo --link elasticsearch:elasticsearch -e "BASE_URL=https://decisions.dcentproject.eu" python:2.7 bash -c 'pip install /var/helsinki/dist/helsinki-0.0.1.tar.gz && helsinki --reindex && helsinki'
+  sudo docker run -d -v /var/helsinki/dist:/var/helsinki/dist -p 127.0.0.1:5000:5000 --name helsinki --link mongo:mongo --link elasticsearch:elasticsearch -e "BASE_URL=https://decisions.dcentproject.eu" --env-file=/var/helsinki/config/config.env python:2.7 bash -c 'pip install /var/helsinki/dist/helsinki-0.0.1.tar.gz && helsinki --reindex && helsinki'
 EOF
