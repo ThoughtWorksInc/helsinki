d63da2b701daab0ad5956339565c90e0feaff1be
John Cowie <jcowie@thoughtworks.com>
2015-08-20 15:48:04 +0100

    JC | Added code for generating env file

--- helsinki/config.py
+++ helsinki/config.py
@@ -1,6 +1,5 @@
 import os
 
-
 hackpad_api_key = os.getenv('HACKPAD_API_KEY')
 hackpad_api_secret = os.getenv('HACKPAD_API_SECRET')
 
@@ -15,3 +14,26 @@ class Config:
 
     def get_hackpad_api_secret(self):
         return hackpad_api_secret
+
+
+envs = ['HACKPAD_API_KEY', 'HACKPAD_API_SECRET']
+
+
+def create_env_file():
+    f = open('config.env', 'w')
+    s = ''
+    for e in envs:
+        s += env_entry(e)
+    f.write(s)
+    f.close()
+
+
+def env_entry(env):
+    value = os.getenv(env)
+    if not value:
+        raise Exception('The environment variable %s needs to be defined' % env)
+    return "%s=%s\n" % (env, value)
+
+
+if __name__ == "__main__":
+    create_env_file()
